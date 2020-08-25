98398bcdb61979e74016cdc6c4ddb0b4049e17bb
Jorge Durban <jdurban@grupomeridian.com>
2020-08-25 09:15:15 +0200

    Added sample of proposed solution during interview

---
+++ ProposedSolution/EmailSanitizer.cs
@@ -0,0 +1,16 @@
+﻿using dotnettest.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotnettest.ProposedSolution
+{
+    public class EmailSanitizer : IOrderSanitizer
+    {
+        public OrderDto Sanitize(OrderDto orderDto)
+        {
+            orderDto.Email = new Email("Dog@gmail.com");
+            return orderDto;
+        }
+    }
+}
---
+++ ProposedSolution/IOrderSanitizer.cs
@@ -0,0 +1,12 @@
+﻿using dotnettest.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotnettest.ProposedSolution
+{
+    public interface IOrderSanitizer
+    {
+        OrderDto Sanitize(OrderDto orderDto);
+    }
+}
